a8f0bb7b396528093877154276fb15a3eaac7e28
Aditya-567 <lextrone000567@gmail.com>
2026-03-01 20:36:16 +0530

    refactor: remove unused build configuration from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -9,23 +10,5 @@ export default defineConfig({
       '3d-solar-system-globe': resolve(__dirname, 'node_modules/3d-solar-system-globe/dist/index.js')
     }
   },
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.js'), // Points to the file we created in Phase 1
-      name: 'PlanetLibrary', // The global variable name (for UMD builds)
-      fileName: 'index',     // The output file name
-    },
-    rollupOptions: {
-      // Externalize dependencies that shouldn't be bundled into your library
-      external: ['react', 'react-dom', 'three', 'react-router-dom', '3d-solar-system-globe'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-          three: 'THREE',
-          'react-router-dom': 'ReactRouterDOM'
-        },
-      },
-    },
-  },
+
 })
